8434459abf3824060241e5f71a7f0c5b349f65a6
Yurimjj <ouo16772@gmail.com>
2025-08-26 11:24:34 +0900

    로그아웃 페이지, 라우터 수정

--- PetCycle 상품+게시판+공지+댓글/PetCycle/client/src/pages/user/LogoutPage.jsx
+++ PetCycle 상품+게시판+공지+댓글/PetCycle/client/src/pages/user/LogoutPage.jsx
@@ -1,30 +1,16 @@
-// src/router/UserRouter.jsx
-import { lazy, Suspense } from "react";
+import LogoutComponent from "../../components/user/LogoutComponent";
+import BasicMenu from "../../components/menus/BasicMenu";
 
-const Loading = <div>Loading...</div>;
+const LogoutPage = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <BasicMenu />
 
-const Login = lazy(() => import("../pages/user/LoginPage"));
-const Logout = lazy(() => import("../pages/user/LogoutPage"));
+      <main className="flex-1 flex items-center justify-center">
+        <LogoutComponent />
+      </main>
+    </div>
+  );
+};
 
-const UserRouter = () => [
-  {
-    path: "login", // 부모 라우트가 /user 라면 최종 경로는 /user/login
-    element: (
-      <Suspense fallback={Loading}>
-        <Login />
-      </Suspense>
-    ),
-  },
-
-  // logout 라우터 추가 .. 
-  {
-    path: "logout", // 최종 경로: /user/logout
-    element: (
-      <Suspense fallback={Loading}>
-        <Logout />
-      </Suspense>
-    ),
-  },
-];
-
-export default UserRouter;
+export default LogoutPage;
--- PetCycle 상품+게시판+공지+댓글/PetCycle/client/src/router/UserRouter.jsx
+++ PetCycle 상품+게시판+공지+댓글/PetCycle/client/src/router/UserRouter.jsx
@@ -1,15 +1,28 @@
 // src/router/UserRouter.jsx
 import { lazy, Suspense } from "react";
-import React from "react";
 
 const Loading = <div>Loading...</div>;
+
 const Login = lazy(() => import("../pages/user/LoginPage"));
+const Logout = lazy(() => import("../pages/user/LogoutPage"));
 
 const UserRouter = () => [
-    {
-        path: "login",
-        element: <Suspense fallback={Loading}><Login /></Suspense>,
-    },
+  {
+    path: "login", // 부모 라우트가 /user 라면 최종 경로는 /user/login
+    element: (
+      <Suspense fallback={Loading}>
+        <Login />
+      </Suspense>
+    ),
+  },
+  {
+    path: "logout", // 최종 경로: /user/logout
+    element: (
+      <Suspense fallback={Loading}>
+        <Logout />
+      </Suspense>
+    ),
+  },
 ];
 
 export default UserRouter;
