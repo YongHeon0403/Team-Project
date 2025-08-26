7d2783a131d0e76071df4181c19c5cc33d96c34f
Yurimjj <ouo16772@gmail.com>
2025-08-26 11:21:22 +0900

    로그아웃 추가

--- PetCycle 상품+게시판+공지+댓글/PetCycle/client/src/components/menus/BasicMenu.jsx
+++ PetCycle 상품+게시판+공지+댓글/PetCycle/client/src/components/menus/BasicMenu.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
+import useCustomLogin from "../../hooks/useCustomLogin";
 
 const BasicMenu = () => {
   const loginState = useSelector((state) => state.LoginSlice);
+
+  const { doLogout, moveToPath } = useCustomLogin();
+
+  const handleLogout = () => {
+    doLogout(); // 토큰/스토어 정리
+    alert("로그아웃 되었습니다."); // 알림 원하면 사용
+    moveToPath("/"); // 메인으로 즉시 이동(replace)
+  };
 
   return (
     <nav className="bg-blue-600 text-white">
@@ -44,14 +53,6 @@ const BasicMenu = () => {
               <li>
                 <Link
                   className="text-xl font-bold hover:text-blue-300"
-                  to="/todo/"
-                >
-                  Todo
-                </Link>
-              </li>
-              <li>
-                <Link
-                  className="text-xl font-bold hover:text-blue-300"
                   to="/pet/list"
                 >
                   펫 프로필
@@ -71,12 +72,13 @@ const BasicMenu = () => {
               Login
             </Link>
           ) : (
-            <Link
+            <button
+              type="button"
+              onClick={handleLogout}
               className="bg-yellow-400 text-black font-medium px-4 py-2 rounded hover:bg-yellow-300"
-              to="/user/logout"
             >
               Logout
-            </Link>
+            </button>
           )}
         </div>
       </div>
---
+++ PetCycle 상품+게시판+공지+댓글/PetCycle/client/src/pages/user/LogoutPage.jsx
@@ -0,0 +1,30 @@
+// src/router/UserRouter.jsx
+import { lazy, Suspense } from "react";
+
+const Loading = <div>Loading...</div>;
+
+const Login = lazy(() => import("../pages/user/LoginPage"));
+const Logout = lazy(() => import("../pages/user/LogoutPage"));
+
+const UserRouter = () => [
+  {
+    path: "login", // 부모 라우트가 /user 라면 최종 경로는 /user/login
+    element: (
+      <Suspense fallback={Loading}>
+        <Login />
+      </Suspense>
+    ),
+  },
+
+  // logout 라우터
+  {
+    path: "logout", // 최종 경로: /user/logout
+    element: (
+      <Suspense fallback={Loading}>
+        <Logout />
+      </Suspense>
+    ),
+  },
+];
+
+export default UserRouter;
